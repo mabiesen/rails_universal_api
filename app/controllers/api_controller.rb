836ddb44b766996ce68c892e9628ff0363a1eefd
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-08-29 14:28:14 -0500

    passing positive spec for call

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,7 @@
 
 class ApiController < ApplicationController
   before_action :set_arguments, only: [:call]
+  before_action :set_endpoint, only: [:call]
 
   # returns list of endpoints
   def list_endpoints
@@ -11,19 +12,27 @@ class ApiController < ApplicationController
 
   # makes an api request
   def call
-    @endpoint = Endpoint.where(name: params[:request_name])
-                        .where(client_tag: params[:client_tag]).first
     raise 'That endpoint does not exist' if @endpoint.nil?
 
-    endpoint_client = EndpointClient.new(@endpoint)
-    response = endpoint_client.request(@arguments)
-    respond_to do |format|
-      format.json { render json: { status: response.status, body: response.body }, status: 200}
-    end
+    response = make_request
+    render json: { status: response.status, body: response.body }, status: 200
   end
 
   private
 
+  # executes the api request
+  def make_request
+    endpoint_client = EndpointClient.new(@endpoint)
+    endpoint_client.request(@arguments)
+  end
+
+  # endpoint is defined by its client and name
+  def set_endpoint
+    @endpoint = Endpoint.where(name: params[:request_name])
+                        .where(client_tag: params[:client_tag]).first
+  end
+
+  # array of arguments to be used in client call
   def set_arguments
     @arguments = params[:arguments]
   end
