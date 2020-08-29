7551ad3808fb6f6765865d34526d96eb66774694
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-08-29 12:36:29 -0500

    major changes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApiController < ApplicationController
-  before_action :set_arguments, only: [:request]
+  before_action :set_arguments, only: [:call]
 
   # returns list of endpoints
   def list_endpoints
@@ -12,7 +12,7 @@ class ApiController < ApplicationController
   end
 
   # makes an api request
-  def request
+  def call
     @endpoint = Endpoint.where(name: params[:request_name])
                         .where(client_tag: params[:client_tag]).first
     raise 'That endpoint does not exist' if @endpoint.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/list_endpoints', to: 'api#list_endpoints'
-  get '/request/:client_tag/:request_name', to: 'api#request'
+  default_format = { :format => 'json' }
+  get '/list_endpoints', to: 'api#list_endpoints', :defaults => default_format
+  post '/call/:client_tag/:request_name', to: 'api#call', :defaults => default_format
 end
