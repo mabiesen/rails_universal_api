666647b16517e5b3716589648a426b0fd9526eaa
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-08-29 12:56:35 -0500

    add spec for controller

---
+++ spec/controllers/api_controller_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+describe ApiController, type: :controller do
+
+  describe '#list_endpoints' do
+    context 'when called' do
+      it 'should return json containing all available endpoints' do
+
+      end
+    end
+
+    context 'when error with call' do
+      it 'should return json indicating error occurred' do
+
+      end
+    end
+  end
+
+  describe '#call' do
+    context 'when called' do
+      it 'should return json output for called entity' do
+
+      end
+    end
+
+    context 'when called with error' do
+      it 'should return json output indicating error' do
+
+      end
+    end
+  end
+end
