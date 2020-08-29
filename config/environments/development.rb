826949eb9e923dc5798c626b32bd82732b4a6b5c
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-08-29 00:34:51 -0500

    restore error related concerns

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   # turning off as it overrides api failures - MB
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
