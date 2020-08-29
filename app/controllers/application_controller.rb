15da4dca16ad2d5dacfb4f4ef11fba6a31b58558
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-08-29 00:32:19 -0500

    restore error related concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
-  rescue_from StandardError, with: :render_standard_error
-
-  def render_standard_error(_exception)
-    redirect_to controller: 'errors', action: 'exception'
-  end
+class ApplicationController < ActionController::Base
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/list_endpoints', to: 'api#list_endpoints'
   get '/request/:client_tag/:request_name', to: 'api#request'
-
-  match "/404" => "errors#not_found", via: [:get, :post]
-  match "/500" => "errors#exception", via: [:get, :post]
 end
