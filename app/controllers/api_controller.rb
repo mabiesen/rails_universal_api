360422218952ae423f3d34c85aba9ad64dcfc632
Matthew Biesen <mabiesen@outlook.com>
2020-08-30 15:13:29 -0500

    ability to filter by like client name or tag (#16)
    * add ability to filter by like client name or tag
    
    Co-authored-by: Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,11 +3,11 @@
 class ApiController < ApplicationController
   before_action :set_arguments, only: [:call]
   before_action :set_endpoint, only: [:call]
+  before_action :set_endpoints, only: [:list_endpoints]
 
   # returns list of endpoints
   def list_endpoints
-    endpoints = Endpoint.all
-    render json: endpoints, status: 200
+    render json: @endpoints, status: 200
   end
 
   # makes an api request
@@ -33,6 +33,13 @@ class ApiController < ApplicationController
   def set_endpoint
     @endpoint = Endpoint.where(name: params[:request_name])
                         .where(client_tag: params[:client_tag]).first
+  end
+
+  # list of endpoints like params if params supplied
+  # nil params yield all endpoints
+  def set_endpoints
+    @endpoints = Endpoint.where('client_tag like ?', "%#{params[:client_tag]}%")
+                         .where('name like ?', "%#{params[:request_name]}%")
   end
 
   # array of arguments to be used in client call
