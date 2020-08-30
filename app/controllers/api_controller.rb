165c58552fd399639c2b3fb36329f4172c5826bc
Matthew Biesen <mabiesen@outlook.com>
2020-08-30 16:12:47 -0500

    add benchmark to api calls (#18)
    Co-authored-by: Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'benchmark'
+
 class ApiController < ApplicationController
   before_action :set_arguments, only: [:call]
   before_action :set_endpoint, only: [:call]
@@ -17,8 +19,9 @@ class ApiController < ApplicationController
       render json: { error: error_string }, status: 404
     else
       begin
-        response = make_request
-        render json: { status: response.status, body: response.body }, status: 200
+        response = nil
+        benchmark = Benchmark.measure { response = make_request }
+        render json: { benchmark: benchmark, status: response.status, body: response.body }, status: 200
       rescue StandardError => e
         render json: { error: e.to_s }, status: 500
       end
