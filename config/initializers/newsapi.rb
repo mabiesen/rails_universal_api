92a959bd9ec17c6e41f0ba250aa1e72c0a7e1a3f
Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>
2020-08-30 01:29:33 -0500

    changes

--- config/initializers/newsapi.rb
+++ config/initializers/newsapi.rb
@@ -7,5 +7,5 @@ NEWSAPI_CLIENT = Faraday.new(NEWSAPI_CONFIG['url']) do |f|
   f.request :retry
   f.response :json, :content_type => /\bjson$/
   f.adapter :net_http
-  f.headers['X-Api-Key'] = '06b5d80da88c4d3d9ba3aeace741c1db'
+  f.headers['X-Api-Key'] = ENV['NEWSAPI_KEY']
 end
