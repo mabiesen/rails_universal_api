4aa345ad8ddf69c7fe567a7dc28d371a6272875e
Matthew Biesen <mabiesen@outlook.com>
2020-08-30 04:47:04 -0500

    Prefer hashed inputs (#11)
    * allow hashed parameter inputs
    
    Co-authored-by: Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>

--- spec/interactors/endpoint_request_builder_spec.rb
+++ spec/interactors/endpoint_request_builder_spec.rb
@@ -11,18 +11,21 @@ RSpec.describe EndpointRequestBuilder do
     context 'when field data supplied cannot be coerced to parameter type' do
       it 'should raise an error' do
         expect{ builder.validate(['arg1','arg2']) }.to raise_error(/is not a valid/)
+        expect{ builder.validate({things: 'stuff', stuff: 'stuff'}) }.to raise_error(/is not a valid/)
       end
     end
 
     context 'when field data is not optional and there is no data supplied' do
       it 'should raise an error' do
         expect{ builder.validate(['2020-09-13']) }.to raise_error(/is not optional/)
+        expect{ builder.validate({things: '2020-09-13'}) }.to raise_error(/is not optional/)
       end
     end
 
     context 'when perfect' do
       it 'should not raise errors' do
         expect{ builder.validate(['2020-02-04','2020-10-29']) }.not_to raise_error
+        expect{ builder.validate({things: '2020-02-04', stuff: '2020-10-29'}) }.not_to raise_error
       end
     end
   end
@@ -33,6 +36,15 @@ RSpec.describe EndpointRequestBuilder do
         data_array = ['2020-02-04','2020-10-29']
         formatted_url_path = builder.formatted_url_path(data_array)
         expect(formatted_url_path).to include(data_array[0])
+        expect(formatted_url_path).not_to include(':things')
+        expect(formatted_url_path).to include('stuff')
+      end
+    end
+    context 'when supplied a hash of data' do
+      it 'returns the interpolated url_path' do
+        data_hash = {things: '2020-02-04', stuff: '2020-10-29'}
+        formatted_url_path = builder.formatted_url_path(data_hash)
+        expect(formatted_url_path).to include(data_hash[:things])
         expect(formatted_url_path).not_to include(':things')
         expect(formatted_url_path).to include('stuff')
       end
@@ -51,6 +63,14 @@ RSpec.describe EndpointRequestBuilder do
     context 'when supplied an array of data with count greater than url variable count' do
       it 'returns hash containing data converted to param type' do
         extra_params = builder.extra_params(['2020-02-04', '2020-10-29'])
+        expect(extra_params).to be_a(Hash)
+        expect(extra_params['stuff']).to eq('2020-10-29')
+      end
+    end
+
+    context 'when supplied a hash of data with key count greater than url variable count' do
+      it 'returns hash containing leftover data converted to param type' do
+        extra_params = builder.extra_params({things: '2020-02-04', stuff: '2020-10-29'})
         expect(extra_params).to be_a(Hash)
         expect(extra_params['stuff']).to eq('2020-10-29')
       end
