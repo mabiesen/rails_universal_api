fdcc01989bfdb3399bbe290550bbce84b3d6a99f
Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>
2020-08-30 19:40:02 -0500

    change

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+require 'codecov'
+
+SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
