843b36c1a4b463fad8589b2ad11407320889fb4f
Matthew Biesen <mbiesen@clm-mbiesen.vpn.enova.com>
2020-09-02 02:41:05 -0500

    add index controller spec

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe HomeController, type: :controller do
+  let(:github_endpoint ) { FactoryBot.create(:endpoint) }
+
+  before(:each) do
+    github_endpoint.save!
+  end
+
+  describe '#index' do
+    it 'should render html' do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq("text/html")
+    end
+  end
+
+end
