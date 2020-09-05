f446221c9da11e123de405a2a36cf1b24040d573
Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>
2020-09-05 00:43:34 -0500

    upgrade

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'dotenv' ; Dotenv.overload ".env.local", ".env.#{Rails.env}"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  default_format = { :format => 'json' }
-  post '/list_endpoints', to: 'api#list_endpoints', :defaults => default_format
-  post '/call/:client_tag/:request_name', to: 'api#call', :defaults => default_format
-  post '/validate_params/:client_tag/:request_name', to: 'api#validate_params', :defaults => default_format
-  post '/validate_param/:client_tag/:request_name', to: 'api#validate_param', :defaults => default_format
 end
