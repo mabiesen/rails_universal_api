033ed8fe834f232ee2e1dc5248cd89b2c48893f9
Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>
2020-09-07 02:06:43 -0500

    alter routes and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,16 @@
 require 'awesome_print'
 
 class HomeController < ApplicationController
-  before_action :set_endpoint, only:  %i[endpoint]
+  before_action :set_endpoint, only:  %i[endpoint_page]
 
   def index
     @endpoints = Endpoint.all
   end
 
-  def endpoint
-    @endpoint
+  def endpoint_page
+    render_home_request do
+      render("endpoint_page")
+    end
   end
 
   private
@@ -21,6 +23,20 @@ class HomeController < ApplicationController
     else
       @endpoint = Endpoint.where(name: params[:request_name])
                           .where(client_tag: params[:client_tag]).first
+    end
+  end
+
+  # wraps requests that contain endpoint instantiation to insure 404 not found
+  def render_home_request
+    if @endpoint.nil?
+      error_string = "no endpoint was found for client: #{params[:client_tag]} with request: #{params[:request_name]}"
+      render json: { error: error_string }, status: 404
+    else
+      begin
+        yield
+      rescue StandardError => e
+        render json: { error: e.to_s }, status: 500
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # UI related
 
   get '/', to: 'home#index'
-  get 'endpoint/:endpoint_id', to: 'home#endpoint'
-  get 'endpoint/:client_tag/:request_name', to: 'home#endpoint'
+  get '/home/endpoint/:endpoint_id', to: 'home#endpoint_page'
+  get '/home/endpoint/:client_tag/:request_name', to: 'home#endpoint_page'
 
 
   # API related
