79697b07daf4d6dc36a114d4f7f02668ba11f03d
Matthew Biesen <mbiesen@clm-mbiesen.attlocal.net>
2020-09-06 21:57:28 -0500

    add datatables

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("datatables.net-bs4")
 
 import './boostrap_custom.js';
+import './datatable.js'
 import './fontawesome_custom.js';
 import '../stylesheets/application';
 
